0823355de235f3c91054401f4e759ff2215ecd76
Krishna Agrawal <krishnaagrawal2304@gmail.com>
2024-10-06 16:01:55 +0530

    added linear search code in java

---
+++ Vectors/LinearSearch/LinearSearch.java
@@ -0,0 +1,23 @@
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i; 
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int key = 30;
+
+        int result = linearSearch(arr, key);
+
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+}
