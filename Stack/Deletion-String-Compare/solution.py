46548a97930494639e29a20f5ae90c19d843046f
negativenagesh <gaonkarsub@gmail.com>
2024-10-07 16:01:51 +0530

    modified solution.py

--- Stack/Deletion-String-Compare/solution.py
+++ Stack/Deletion-String-Compare/solution.py
@@ -1,30 +1,22 @@
-def are_strings_identical(s, t):
-    # Let's define a helper function to handle the string processing
-    def process_string(s):
-        stack = []  # We'll use a stack to simulate our text editor
-        for char in s:
-            if char == '#':
-                # If we see a backspace, check if our stack isn't empty
-                if stack:
-                    stack.pop()  # Remove the last character if there's one to remove
+class string(object):
+    def hashtag(self,str1,str2):
+
+        str1=[]
+        str2=[]
+
+        for char in str1:            
+            if char =='#':
+                if str1 !=[]:
+                    str1.pop()
+
             else:
-                # If it's a regular character, just add it to the stack
-                stack.append(char)
-        # Join everything in the stack to get the final processed string
-        return ''.join(stack)
+                str1.append(char)
 
-    # Now, let's process both strings and see if they're identical
-    return process_string(s) == process_string(t)
-
-# Example usage:
-s1 = "xy#z"
-t1 = "xz#z"
-print(are_strings_identical(s1, t1))  # Should output: True
-
-s2 = "a##"
-t2 = "b##"
-print(are_strings_identical(s2, t2))  # Should output: True
-
-s3 = "a#b"
-t3 = "x"
-print(are_strings_identical(s3, t3))  # Should output: False
+        for char in str2:
+            if char =='#':
+                if str2 !=[]:
+                    str2.pop()
+            
+            else:
+                str2.append(char)
+        return str1==str2
